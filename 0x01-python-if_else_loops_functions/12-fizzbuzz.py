6efd686566338babb35726df8dd0ed02a48a23fe
mitch codes <mitchkodes@gmail.com>
2022-06-01 08:56:55 -0700

    higher level

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -6,7 +6,7 @@ for i in range(1, 101):
         print("Buzz", end="")
     else:
         print("{:d}".format(i), end="")
-    if i != 100:
+    if i < 100:
         print(" ", end="")
     else:
         print("")
