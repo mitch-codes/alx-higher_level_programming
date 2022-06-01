6fbc9b5d02b720f757b5e8b0302551ce932e0af4
mitch codes <mitchkodes@gmail.com>
2022-05-31 18:06:06 -0700

    higher level

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 for i in range(100):
-    print("{:d}".format(i), end="")
     if i < 99:
-        print(", ", end="")
+        if i < 10:
+            print("0{:d}, ".format(i), end="")
+        else:
+            print("{:d}, ".format(i), end="")
     else:
-        print("")
+        print("{:d}".format(i))
