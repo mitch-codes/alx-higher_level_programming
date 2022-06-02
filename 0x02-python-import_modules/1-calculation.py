2fe553c2923a974b84bfec3a2b0e27c5adef2acb
mitch codes <mitchkodes@gmail.com>
2022-06-02 06:30:39 -0700

    higher level

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import calculator_1
-a = 10
-b = 5
-print("{:d}".format(calculator_1.add(a, b)))
-print("{:d}".format(calculator_1.sub(a, b)))
-print("{:d}".format(calculator_1.mul(a, b)))
-print("{:d}".format(calculator_1.add(a, b)))
+if __name__ == "__main__":
+    import calculator_1
+    a = 10
+    b = 5
+    print("{:d} + {:d} = {:d}".format(a, b, calculator_1.add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, calculator_1.sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, calculator_1.mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, calculator_1.div(a, b)))
