8e18ef51b0ddf5838a59ee06aa533d10ae4839e1
mitch codes <mitchkodes@gmail.com>
2022-06-02 17:16:43 -0700

    higher level

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
-import sys
-for i in range(len(sys.argv)):
-    print("{:d}: {}".format(i, sys.argv[i]))
+if __name__ == "__main__":
+    import sys
+    length = len(sys.argv)
+    arguments = len(sys.argv) - 1
+    if arguments == 1:
+        print("{:d} argument:".format(arguments))
+    else:
+        print("{:d} arguments:".format(arguments))
+    for i in range(1, length):
+        print("{:d}: {}".format(i, sys.argv[i]))
