c614fe6e51175e35a40e607c1b128bfbaefcbb76
mitch-codes <88305630+mitch-codes@users.noreply.github.com>
2022-06-06 17:31:00 +0300

    Update main.py

--- 0x03-python-data_structures/main.py
+++ 0x03-python-data_structures/main.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-from divisible_by_2 import divisible_by_2
-my_list = [0, 1, 2, 3, 4, 5, 6]
-list_result = divisible_by_2(my_list)
+from delete_at import delete_at
 
-i = 0
-while i < len(list_result):
-    print("{:d} {:s} divisible by 2".format(my_list[i], "is" if list_result[i] else "is not"))
-    i += 1
+my_list = [1, 2, 3, 4, 5]
+idx = 3
+new_list = delete_at(my_list, idx)
+print(new_list)
