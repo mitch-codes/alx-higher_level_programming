0eee730f54adeea1de6443dc8f3345a990cba11e
mitch-codes <88305630+mitch-codes@users.noreply.github.com>
2022-06-06 13:47:01 +0300

    Update main.py

--- 0x03-python-data_structures/main.py
+++ 0x03-python-data_structures/main.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 from element_at import element_at
-element_at([1, 2, 3, 4], 1)
+my_list = [1, 2, 3, 4, 5]
+idx = 3
+print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
