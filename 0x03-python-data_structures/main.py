8312ecd0d2219e751631b68c93b98e91b22f0173
mitch codes <mitchkodes@gmail.com>
2022-06-06 03:55:30 -0700

    higher level

--- 0x03-python-data_structures/main.py
+++ 0x03-python-data_structures/main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from element_at import element_at
+from replace_in_list import replace_in_list
 my_list = [1, 2, 3, 4, 5]
 idx = 3
 print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
