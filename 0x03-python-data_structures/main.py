393479b4e4c08d8c8513d5036ab411adc7fabfbc
mitch-codes <88305630+mitch-codes@users.noreply.github.com>
2022-06-06 15:42:49 +0300

    Update main.py

--- 0x03-python-data_structures/main.py
+++ 0x03-python-data_structures/main.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-from replace_in_list import replace_in_list
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_element = 9
-new_list = replace_in_list(my_list, idx, new_element)
+from divisible_by_2 import divisible_by_2
+my_list = [0, 1, 2, 3, 4, 5, 6]
+list_result = divisible_by_2(my_list)
 
-print(new_list)
-print(my_list)
+i = 0
+while i < len(list_result):
+    print("{:d} {:s} divisible by 2".format(my_list[i], "is" if list_result[i] else "is not"))
+    i += 1
