2912f58318e0cf66dcd2f990caaee0837b70556d
mitch-codes <88305630+mitch-codes@users.noreply.github.com>
2022-06-06 13:56:42 +0300

    Update main.py

--- 0x03-python-data_structures/main.py
+++ 0x03-python-data_structures/main.py
@@ -2,4 +2,8 @@
 from replace_in_list import replace_in_list
 my_list = [1, 2, 3, 4, 5]
 idx = 3
-print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
+new_element = 9
+new_list = replace_in_list(my_list, idx, new_element)
+
+print(new_list)
+print(my_list)
