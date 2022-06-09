cbd17f95073c81016517fa5cea0ab6a5cccec8fb
mitch codes <mitchkodes@gmail.com>
2022-06-08 17:59:23 -0700

    higher level

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = [0] * len(my_list)
+    for i in range(len(my_list)):
+        if my_list[i] == search:
+            new_list[i] = replace
+        else:
+            new_list[i] = my_list[i]
+    return new_list
+
