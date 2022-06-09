ac1ce54d98206fca7db91fa9ec0cfb323356819b
mitch codes <mitchkodes@gmail.com>
2022-06-08 17:51:09 -0700

    higher level

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,7 +3,7 @@ def square_matrix_simple(matrix=[]):
     new_matrix = [0] * len(matrix)
     for i in range(len(matrix)):
         new_matrix[i] = [0] * len(matrix[0])
-    for k in range (len(new_matrix)):
-        for l in range(len(new_matrix[k])):
-            new_matrix[k][l] = matrix[k][l]**2
+    for k in range(len(new_matrix)):
+        for el in range(len(new_matrix[k])):
+            new_matrix[k][el] = matrix[k][el]**2
     return new_matrix
