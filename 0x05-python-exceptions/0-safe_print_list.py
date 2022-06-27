11306b4756e34933ff94b049ed7e9ba1d781e313
mitch codes <mitchkodes@gmail.com>
2022-06-26 19:06:07 -0700

    monty interclear

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,4 +7,6 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]))
             j = j + 1
         except IndexError:
-    return j
+            return j
+        finally:
+            return j
