ce72b498bc5f1ace6e6dc2f822c596e3ffebda72
mitch codes <mitchkodes@gmail.com>
2022-06-28 18:49:53 -0700

    python classes

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""a class square that initializes instance variables"""
+"""a class square that defines a square by:
+    (based on 0-square.py)
+
+    """
 
 
 class Square:
     """square class with a private attribute -
     size.
+
     """
     __size
     def __init__(self, size):
