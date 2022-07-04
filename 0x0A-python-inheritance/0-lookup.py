f05c71777c8a35494c0996f1f15933cc2d2a6221
mitch codes <mitchkodes@gmail.com>
2022-07-04 03:50:39 -0700

    python inheritance

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""author: Mitch"""
 
 def lookup(obj):
-    """function that finds attributes of class"""
+    """function that finds attributes of class
+    given an object
+
+    """
 
     return dir(obj)
