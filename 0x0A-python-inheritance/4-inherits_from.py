1c6c95bccf5dfc5e9c02fb228b51609fd121baa2
mitch codes <mitchkodes@gmail.com>
2022-07-04 05:23:38 -0700

    python inheritance

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""author: Mitch"""
+
+
+def inherits_from(obj, a_class):
+    """a function to determine if an object
+    is an inherited from a class
+
+    """
+    result = issubclass(obj, a_class)
+    return result
