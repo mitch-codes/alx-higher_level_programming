a07de1dd6ce6412e0e37bfeb0f893cd5aaf90432
mitch codes <mitchkodes@gmail.com>
2022-07-05 16:41:32 -0700

    reading and writing files

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a method to read files"""
+
+
+def read_file(filename=""):
+    """read file in utf-8"""
+
+    with open(filename, r, encoding = "UTF-8") as f:
+        my_data = f.read()
+        print(my_data)
