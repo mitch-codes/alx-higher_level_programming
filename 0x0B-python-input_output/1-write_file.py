ecd8ea205dc8c17644a0472b330192404249a180
mitch codes <mitchkodes@gmail.com>
2022-07-05 17:01:26 -0700

    reading and writing files

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a function to write into files files"""
+
+
+def read_file(filename="", text=""):
+    """write content into file file in utf-8"""
+
+    with open(filename, mode='w', encoding="UTF-8") as f:
+        my_data = f.write(text)
+        return my_data
