028281dc8fca2db3f70f8adb8bfc9b271d22a429
mitch codes <mitchkodes@gmail.com>
2022-07-05 17:50:56 -0700

    reading and writing files

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""using json to convert text object into json format and write it into a file"""
+"""using json to convert text object into
+json format and write it into a file
+
+"""
 import json
 
 
