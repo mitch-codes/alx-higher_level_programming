3c20a67b961e0581495f032c5f02343d51ff05fa
mitch codes <mitchkodes@gmail.com>
2022-07-12 00:16:24 -0700

    summarise python

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -13,7 +13,7 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """set attributes
-        
+
         Args:
             width(int): value of width
             height(int): value of height
@@ -52,11 +52,11 @@ class Rectangle(Base):
     def width(self):
         """getter returns width"""
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         """setter gets width
-        
+
         Args:
             value(int): value of width
 
@@ -66,16 +66,16 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("width must > 0")
         self.__width = value
-    
+
     @property
     def height(self):
         """getter returns width"""
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         """setter sets width
-        
+
         Args:
             value(int): value of height
 
@@ -85,16 +85,16 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("height must > 0")
         self.__height = value
-    
+
     @property
     def x(self):
         """getter returns width"""
         return self.__x
-    
+
     @x.setter
     def x(self, value):
         """setter gets widt
-        
+
         Args:
             value(int): x value
 
@@ -104,12 +104,12 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("x must > 0")
         self.__x = value
-    
+
     @property
     def y(self):
         """getter returns width"""
         return self.__y
-    
+
     @y.setter
     def y(self, value):
         """setter gets width
