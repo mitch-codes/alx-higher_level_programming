f4d84317ed110a3de79fb0f1805a35f344e1fecd
mitch-codes <88305630+mitch-codes@users.noreply.github.com>
2022-08-23 06:36:06 +0300

    document class

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,10 +5,7 @@
 
 
 class Square:
-    """Square class with a private attribute -
-    size.
-
-    """
+    """Square class with a private attribute size."""
 
     def __init__(self, size=0, position=(0, 0)):
         """Initializes the size variable as a private
