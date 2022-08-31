75e76e39f92100b43b14685cb84b50af2b523263
mitch-codes <88305630+mitch-codes@users.noreply.github.com>
2022-09-01 02:56:49 +0300

    change order

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- create database and user
-CREATE DATABASE IF NOT EXITS 'hbtn_0d_2'
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE DATABASE IF NOT EXITS 'hbtn_0d_2'
 GRANT SELECT on 'hbtn_0d_2' to 'user_0d_2'@'localhost';
