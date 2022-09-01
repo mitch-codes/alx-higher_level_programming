67dba7d9746b20ee20f5c8c9c0827d0ee64841b2
mitch-codes <88305630+mitch-codes@users.noreply.github.com>
2022-09-01 04:21:36 +0300

    change

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,3 @@
+-- create database and table simultaneously
+CREATE DATABASE hbtn_0d_usa;
+CREATE TABLE hbtn_0d_usa.states (id INTEGER NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
