b39b526a4585064ed034c1650576d303eb538204
mitch-codes <88305630+mitch-codes@users.noreply.github.com>
2022-09-01 03:14:17 +0300

    change

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,3 +2,4 @@
 CREATE DATABASE IF NOT EXITS 'hbtn_0d_2';
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON 'hbtn_0d_2' to 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+FLUSH PRIVILEGES;
