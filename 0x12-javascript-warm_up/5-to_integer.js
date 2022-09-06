bf856fe14a893737422ebc0e391c74c9750a76bd
mitch codes <mitchkodes@gmail.com>
2022-09-06 03:41:53 +0000

    javascript

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,7 @@
 #!/usr/bin/nodejs
 const a = parseInt(process.argv[2]);
-if (isNaN(a))
-{
-console.log('Not a number');
-}
-else
-{
-console.log('My number: ' + a);
+if (isNaN(a)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + a);
 }
