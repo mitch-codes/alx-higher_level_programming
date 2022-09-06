d4a5c1c2b0130af5b1a4cf014f621a6fb801fcb7
mitch codes <mitchkodes@gmail.com>
2022-09-06 03:40:09 +0000

    javascript

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,10 +1,7 @@
 #!/usr/bin/nodejs
 
-if (process.argv.length < 3) 
-{
-	console.log('No argument');
-}
-else
-{
-	console.log(process.argv[2]);
+if (process.argv.length < 3) {
+  console.log('No argument');
+} else {
+  console.log(process.argv[2]);
 }
