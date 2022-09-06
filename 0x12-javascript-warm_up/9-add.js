54c49c76bd85d1c322e9fd4219af518a1274bc86
mitch-codes <88305630+mitch-codes@users.noreply.github.com>
2022-09-06 09:42:10 +0300

    change syntax

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,7 @@
 #!/usr/bin/nodejs
-function add (a, b)
-{
-const c = parseInt(a) + parseInt(b);
-console.log(c);
+function add (a, b) {
+  const c = parseInt(a) + parseInt(b);
+  console.log(c);
 }
 
 add(process.argv[2], process.argv[3]);
