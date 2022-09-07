be2ef94d02e6686f0628c0969991bd06c141b2d5
mitch-codes <88305630+mitch-codes@users.noreply.github.com>
2022-09-08 02:56:55 +0300

    assign values only if they are present

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,10 @@
 #!/usr/bin/nodejs
 module.exports = class Rectangle {
   constructor (w, h) {
-    this.width = w;
-    this.height = h;
+    let test = w * h;
+    if (test > 0) { 
+      this.width = w;
+      this.height = h;
+    }
   }
 };
