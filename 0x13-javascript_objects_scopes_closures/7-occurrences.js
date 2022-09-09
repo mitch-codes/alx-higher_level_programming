eedcfc7ce6ef0990d95058c14b2b4ce2a2975b30
mitch-codes <88305630+mitch-codes@users.noreply.github.com>
2022-09-09 05:01:24 +0300

    extend function

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,11 @@
+#!/usr/bin/nodejs                                                                                                                       
+exports.nbOccurences = function (list, searchElement) {                                                                                 
+let length = list.length;                                                                                                               
+let count = 0;                                                                                                                          
+for (let i = 0; i < length; i++) {                                                                                                      
+if (list[i] === searchElement) {                                                                                                        
+count = count + 1;                                                                                                                      
+}                                                                                                                                       
+}                                                                                                                                       
+console.log(count);                                                                                                                     
+};
