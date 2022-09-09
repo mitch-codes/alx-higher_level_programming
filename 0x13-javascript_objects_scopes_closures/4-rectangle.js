daca769a1830e706da65f93da844bee726c83a63
mitch-codes <88305630+mitch-codes@users.noreply.github.com>
2022-09-09 03:47:28 +0300

    change syntax

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -19,12 +19,12 @@ module.exports = class Rectangle {
     }
   }
   rotate () {
-  let temp = this.width;
-  this.width = this.height;
-  this.height = temp;
+    let temp = this.width;
+    this.width = this.height;
+    this.height = temp;
   }
   double () {
-  this.width = this.width * 2;
-  this.height = this.height * 2;
+    this.width = this.width * 2;
+    this.height = this.height * 2;
   }
 };
