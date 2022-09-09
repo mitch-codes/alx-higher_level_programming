7202017622830ec7e3f289b21ceae7f82699de08
mitch-codes <88305630+mitch-codes@users.noreply.github.com>
2022-09-09 04:08:34 +0300

    test

--- 0x13-javascript_objects_scopes_closures/0-main.js
+++ 0x13-javascript_objects_scopes_closures/0-main.js
@@ -1,6 +1,7 @@
-#!/usr/bin/node
-const Rectangle = require('./0-rectangle');
+#!/usr/bin/nodejs
+const Square = require('./6-square');
 
-const r1 = new Rectangle();
-console.log(r1);
-console.log(r1.constructor);
+const s1 = new Square(4);
+s1.charPrint();
+
+s1.charPrint('C');
