225ccfd389ea17d8fa3440be9c195b6b2c56f022
mitch codes <mitchkodes@gmail.com>
2022-09-09 00:54:40 +0000

    javascript

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,9 +1,9 @@
 #!/usr/bin/nodejs
 module.exports = class Rectangle {
   constructor (w, h) {
-    let test = w * h;
- 
-    if (test > 0) { 
+    const test = w * h;
+
+    if (test > 0) {
       this.width = w;
       this.height = h;
     }
