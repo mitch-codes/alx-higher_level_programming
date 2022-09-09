5fb6978acf08208de43fc5f5f1f5c0c35989c501
mitch codes <mitchkodes@gmail.com>
2022-09-09 01:02:09 +0000

    javascript

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,8 +1,9 @@
 #!/usr/bin/nodejs
 module.exports = class Square extends require('./4-rectangle.js') {
   constructor (size) {
-    if (size > -1) {
-      super(size, size);
+    if (size < 0) {
+      size = undefined;
     }
+    super (size, size);
   }
 };
