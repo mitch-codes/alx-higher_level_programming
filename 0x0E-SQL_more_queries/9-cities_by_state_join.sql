eb19225aec455f65db3c2d2f25c6dd557265b330
mitch codes <mitchkodes@gmail.com>
2022-09-10 08:03:25 +0000

    sql debbuging

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- select dat from 2 tables
 SELECT cities.id, cities.name, states.name
 FROM cities
-INNER JOIN states ON cities.state_id=sates.id
+INNER JOIN states ON cities.state_id=states.id
 ORDER BY cities.id;
