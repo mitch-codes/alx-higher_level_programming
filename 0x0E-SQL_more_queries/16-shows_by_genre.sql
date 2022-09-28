e3e31475a9fbc6da0169cf78df69c9095dacd73d
mitch codes <mitchkodes@gmail.com>
2022-09-28 00:27:24 +0000

    connect to mysql

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,5 @@
 -- select dat from 2 tables
 SELECT tv_shows.title, tv_genres.name
 FROM tv_shows JOIN tv_show_genres
-ON tv_shows.id=tv_show_genres.show_id JOIN tv_genres
-ON tv_show_genres.genre_id=tv_genres.id
+JOIN tv_genres
 ORDER BY tv_shows.title, tv_genres.name;
