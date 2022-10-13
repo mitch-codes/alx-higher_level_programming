cbab5ebe8bb6195fbc07f7071f7c4fc6adbd89d0
mitch codes <mitchkodes@gmail.com>
2022-10-13 01:09:11 +0000

    connect to mysql

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -7,7 +7,14 @@ from sqlalchemy import Column, Integer, String
 Base = declarative_base()
 
 class State(Base):
-"""map table name and columns"""
+    """State class
+
+    Attributes:
+        __tablename__ (str): The table name of the class
+        id (int): The State id of the class
+        name (str): The State name of the class
+
+    """
     __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True)
