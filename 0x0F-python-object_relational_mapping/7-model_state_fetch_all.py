9573d843828f055de4c72ab1fe2d092973845eca
mitch codes <mitchkodes@gmail.com>
2022-10-14 05:10:54 +0000

    connect using sqlalchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
 
     session = Session(engine)
-    for state in session.query(State):
+    for state in session.query(State).all():
         print("{}: {}".format(state.id, state.name))
     session.close()
 
