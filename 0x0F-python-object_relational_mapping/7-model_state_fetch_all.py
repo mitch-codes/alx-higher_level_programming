82d81c397133aecf4fac32ae847edd032e9a0cf6
mitch codes <mitchkodes@gmail.com>
2022-10-14 05:02:38 +0000

    connect using sqlalchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -6,7 +6,6 @@ import MySQLdb
 from sqlalchemy import create_engine
 import sys
 from model_state import Base, State
-import sys
 
 if __name__ == "__main__":
     engine = sqlalchemy.create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
