bf944dde9b73fe66e2e50fce37c556cfec9563a0
mitch codes <mitchkodes@gmail.com>
2022-10-16 04:41:08 +0000

    connect using sqlalchemy

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""connect to mysql to retrieve
-only one record"""
+"""
+lists all State objects that contain the
+letter a from the database hbtn_0e_6_usa"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
