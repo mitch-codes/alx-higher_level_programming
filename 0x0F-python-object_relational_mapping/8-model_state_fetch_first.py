1f81a4a9fab64cde1aea363ce42bbfa0b4cec888
mitch codes <mitchkodes@gmail.com>
2022-10-16 04:33:25 +0000

    connect using sqlalchemy

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,17 +1,14 @@
-
 #!/usr/bin/python3
-"""
-script that lists all State objects
-from the database hbtn_0e_6_usa"""
+"""connect to mysql to retrieve
+only one record"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from model_state import Base, State
 
-
 if __name__ == "__main__":
     engine = create_engine(
-                            "mysql+mysqldb://{}:{}@localhost/{}"
+                            'mysql+mysqldb://{}:{}@localhost/{}'
                             .format(
                                         sys.argv[1],
                                         sys.argv[2],
@@ -20,9 +17,5 @@ if __name__ == "__main__":
                             pool_pre_ping=True
                                 )
     session = Session(engine)
-    re = session.query(State).order_by(State.id).first()
-    if re is None:
-        print("Nothing")
-    else:
-        print("{}: {}".format(re.id, re.name))
-    session.close()
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
