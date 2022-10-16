d0622d1e2c1f237653fd848134edc76631f9f57f
mitch codes <mitchkodes@gmail.com>
2022-10-16 03:55:31 +0000

    connect using sqlalchemy

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -2,22 +2,22 @@
 """"
 display all the records of
 a database table"""
-from model_state import Base, State
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
+from model_state import Base, State
 
 if __name__ == "__main__":
     engine = create_engine(
                             "mysql+mysqldb://{}:{}@localhost/{}"
                             .format(
-                                    sya.argv[1],
+                                    sys.argv[1],
                                     sys.argv[2],
                                     sys.argv[3],
                                         ),
                             pool_pre_ping=True
                                 )
     session = Session(engine)
-    results = session.query(State).order_by(State.id).first()
-    print("{}: {}".format(results.id, results.name))
+    for state in session.query(State).order_by(State.id).first():
+        print("{}: {}".format(state.id, state.name))
     session.close()
