e7d34a0eb9b86cad65314e569b4f43df31c52811
mitch-codes <88305630+mitch-codes@users.noreply.github.com>
2022-12-19 12:40:34 +0300

    test doctest

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -13,4 +13,6 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if (isinstance(b, (int, float)) == False):
         raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
     return (a + b)
