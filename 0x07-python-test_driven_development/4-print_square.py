09049e41e17a8afd57bc8ac79845b7e21e0e6ce1
mitch-codes <88305630+mitch-codes@users.noreply.github.com>
2022-12-20 05:07:27 +0300

    print square

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -4,12 +4,12 @@
 
 def print_square(size):
     """Prints a square with the character '#'"""
-    if type(size) != int:
+    if isinstance(size, (int, float)) is False:
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    for x in range(size):
-        for y in range(size):
-            print("#", end='')
-        print()
-        
+    size = int(size)
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print("\n", end="")
