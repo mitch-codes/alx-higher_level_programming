78e38a00fd98641c4c19b40d80c8709bf6d325e6
mitch-codes <88305630+mitch-codes@users.noreply.github.com>
2022-12-28 12:46:31 +0300

    make changes to the print statements

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,8 +8,9 @@ def safe_print_list(my_list=[], x=0):
     list_count = 0
     for i in range(x):
         try:
-            print(my_list[i])
+            print(my_list[i], end="")
             list_count += 1
         except IndexError:
             break
+    print("")
     return list_count
