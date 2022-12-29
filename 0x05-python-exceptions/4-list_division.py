e9a3050b3a134e0063d8118e40c3fffe15932cbe
mitch-codes <88305630+mitch-codes@users.noreply.github.com>
2022-12-29 13:07:45 +0300

    add except clause

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -6,19 +6,19 @@ def list_division(my_list_1, my_list_2, list_length):
     A function that divides element
     by element 2 lists
     """
-    temp_list = []
-    for i in range(0, list_length):
+    my_list = []
+    for i in range(list_length):
         try:
-            result = my_list_1[i] / my_list_2[i]
+            answer = my_list_1[i] / my_list_2[i]
         except TypeError:
-            print("wrong type")
-            result = 0
+            print ("wrong type")
+            answer = 0
         except ZeroDivisionError:
-            print("division by 0")
-            result = 0
+            print ("division by 0")
+            answer = 0
         except IndexError:
-            print("out of range")
-            result = 0
+            print ("out of range")
+            answer = 0
         finally:
-            temp_list.append(result)
-    return temp_list
+            my_list.append(answer)
+    return (my_list)
