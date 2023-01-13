e69955bbe64f61b4d65092fd4661de7191b76f26
mitch-codes <88305630+mitch-codes@users.noreply.github.com>
2023-01-13 15:42:55 +0300

    override the area method in super class

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -11,4 +11,10 @@ class Rectangle(BaseGeometry):
         super().integer_validator("width", width)
         super().integer_validator("height", height)
         self.__width = width
-        self.__height = width
+        self.__height = height
+
+    def __str__(self):
+        return ("[{}] {}/{}".format(self.__class__.__name__, self.__width, self.__height))
+
+    def area(self):
+        return (self.__width * self.__height)
