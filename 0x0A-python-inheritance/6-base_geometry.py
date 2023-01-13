68a2eb0a659950ffb60ae2fcc752b31fd66de535
mitch-codes <88305630+mitch-codes@users.noreply.github.com>
2023-01-13 14:08:56 +0300

    add method

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,1 +1,9 @@
+#!/usr/bin/python3
+"""author: Mitch"""
 
+
+class BaseGeometry():
+    """ class raising an error"""
+    def area(self):
+        raise Exception("area() is not implemented")
+        
