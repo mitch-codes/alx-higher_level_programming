736687280be3cf2e357518b34cd68b73f4373e4e
mitch-codes <88305630+mitch-codes@users.noreply.github.com>
2023-01-13 15:45:05 +0300

    update the correct file

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -12,9 +12,3 @@ class Rectangle(BaseGeometry):
         super().integer_validator("height", height)
         self.__width = width
         self.__height = height
-
-    def __str__(self):
-        return ("[{}] {}/{}".format(self.__class__.__name__, self.__width, self.__height))
-
-    def area(self):
-        return (self.__width * self.__height)
