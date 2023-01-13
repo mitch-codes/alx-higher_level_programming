4b44f1e0a97d1518ddf59a20fe97579c2b6f7e68
mitch-codes <88305630+mitch-codes@users.noreply.github.com>
2023-01-13 15:26:47 +0300

    change the py syntax

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -3,11 +3,13 @@
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     "an inheritence of Base Geometry"
-    
+ 
+
     def __init__(self, width, height):
         super().integer_validator("width", width)
-        super().integer_validator("height", width)
+        super().integer_validator("height", height)
         self.__width = width
         self.__height = width
