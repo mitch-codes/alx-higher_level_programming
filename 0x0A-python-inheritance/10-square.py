03168647beb06874eee75e550e2ae1d45ad2062f
mitch-codes <88305630+mitch-codes@users.noreply.github.com>
2023-01-13 16:53:02 +0300

    add override methods

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -8,8 +8,13 @@ class Square(Rectangle):
     "an inheritence of class rectangle"
 
     def __init__(self, size):
-        super().__init__(size, size)
+        BaseGeometry.integer_validator(self, "size", size) 
         self.__size = size
+        print (self.__size)
+
+    def __str__(self):
+        return ("[{}] {}/{}"
+        .format(self.__class__.__name__, self.__size, self.__size))
 
     def area(self):
         return (self.__size * self.__size)
