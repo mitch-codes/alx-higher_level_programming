6658f2188b88049cf8841287265d0da1ff014082
mitch-codes <88305630+mitch-codes@users.noreply.github.com>
2023-01-13 15:20:29 +0300

    import without importing

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """author: Mitch"""
 
-from 7-base_geometry import BaseGeometry
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     "an inheritence of Base Geometry"
