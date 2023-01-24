c38d51917923c73c0210a8e6dc60aaabad0e567e
mitch-codes <88305630+mitch-codes@users.noreply.github.com>
2023-01-24 11:10:58 +0300

    join two tables

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,4 @@
 -- select dat from 2 tables
 SELECT cities.id, cities.name, states.name
-FROM cities
-INNER JOIN states ON cities.state_id=states.id
-ORDER BY cities.id;
+FROM cities c NATURAL JOIN states s
+ORDER BY c.id;
