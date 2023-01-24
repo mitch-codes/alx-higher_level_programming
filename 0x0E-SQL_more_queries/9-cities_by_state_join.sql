3549a5ab9753b75a044ffa4a57c20390be18e184
mitch-codes <88305630+mitch-codes@users.noreply.github.com>
2023-01-24 11:34:41 +0300

    TRY LEFT JOIN

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- select dat from 2 tables
 SELECT cities.id, cities.name, states.name
-FROM cities INNER JOIN states
+FROM cities LEFT JOIN states
 ON cities.state_id = states.id
 ORDER BY cities.id;
