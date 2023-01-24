6758f85f30d05649cf4467de6ca1fff0e2df476b
mitch-codes <88305630+mitch-codes@users.noreply.github.com>
2023-01-24 11:34:01 +0300

    change syntax

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- select dat from 2 tables
 SELECT cities.id, cities.name, states.name
-FROM cities c INNER JOIN states s
-ON c.state_id = s.id
-ORDER BY c.id;
+FROM cities INNER JOIN states
+ON cities.state_id = states.id
+ORDER BY cities.id;
