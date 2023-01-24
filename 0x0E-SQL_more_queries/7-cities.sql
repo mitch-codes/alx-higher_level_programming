d919aed869f22fff5e0ed25f7f3cf1302d555d57
mitch-codes <88305630+mitch-codes@users.noreply.github.com>
2023-01-24 10:58:01 +0300

    create table with foreign key constraint

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,9 +1,9 @@
 -- create database and table simultaneously with foreugn key
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-  id INTEGER AUTO_INCREMENT,
-  state_id INTEGER NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (state_id)
-  REFERENCES states(id));
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
+id INT AUTO_INCREMENT PRIMARY KEY,
+state_id INT,
+name VARCHAR(256) NOT NULL,
+FOREIGN KEY(state_id)
+REFERENCES hbtn_0d_usa.states(id)
+);
