3f11047822ac773b2d3388e5c3d8f06a39be6333
mitch-codes <88305630+mitch-codes@users.noreply.github.com>
2023-01-24 10:46:35 +0300

    create table with primary key constraint

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,3 +1,6 @@
 -- create database and table simultaneously
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INTEGER NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
+CREATE TABLE IF NOT EXITS hbtn_0d_usa.states(
+id INT AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(256) NOT NULL
+);
