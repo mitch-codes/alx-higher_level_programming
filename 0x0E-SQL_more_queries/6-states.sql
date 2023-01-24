fab3a8b36c0704fb094560dc51fb640681c2e861
mitch-codes <88305630+mitch-codes@users.noreply.github.com>
2023-01-24 10:52:58 +0300

    change syntax

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- create database and table simultaneously
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXITS hbtn_0d_usa.states(
-id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+id INT AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(256) NOT NULL
 );
