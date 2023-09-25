483a729eb574be878a1317c490d9032c2fe7a90b
MITCHELL OTIENO MONDA <88305630+mitch-codes@users.noreply.github.com>
2023-09-25 12:21:12 +0300

    confirm sql statement

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
 
-    cur.execute("SELECT states.id, cites.name, states.name FROM states INNER JOIN cities WHERE states.id = cities.state_id ORDER BY state.id")
+    cur.execute("SELECT states.id, cities.name, states.name FROM states INNER JOIN cities WHERE states.id = cities.state_id ORDER BY states.id")
     results = cur.fetchall()
     for row in results:
         print(row)
