fa21ae42e575d8ee16e441e0f1f67f799dde9ddc
MITCHELL OTIENO MONDA <88305630+mitch-codes@users.noreply.github.com>
2023-09-25 11:20:26 +0300

    change syntax

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,13 +5,11 @@
 import MySQLdb
 import sys
 
-db = MySQLdb.connect(user=sys.argv[1], passwd=sys.agrv[2], port=3306, db=sys.argv[3])
-cur = db.cursor()
-
-cur.execute("SELECT id, name FROM states WHERE name LIKE %s ORDER BY id",("N%"))
-rows = cur.fetchall()
-for row in rows:
-    print(row)
-
 if __name__ == "__main__":
-    main()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.agrv[2], port=3306, db=sys.argv[3])
+    cur = db.cursor()
+
+    cur.execute("SELECT id, name FROM states WHERE name LIKE %s ORDER BY id",("N%"))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
