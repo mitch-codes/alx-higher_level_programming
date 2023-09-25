3c90619ae2ec4dcd849bc0e84f80971b605f408f
MITCHELL OTIENO MONDA <88305630+mitch-codes@users.noreply.github.com>
2023-09-25 13:41:48 +0300

    import right module

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -4,7 +4,7 @@ script that lists all State objects
 from the database hbtn_0e_6_usa"""
 import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 
