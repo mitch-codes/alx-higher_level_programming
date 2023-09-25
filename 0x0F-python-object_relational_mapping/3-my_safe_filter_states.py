63128405f7c51113b50046a0cbecbfd02885e761
MITCHELL OTIENO MONDA <88305630+mitch-codes@users.noreply.github.com>
2023-09-25 12:11:44 +0300

    avoid sql injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""connect to database python"""
+"""get info from mysql database but avoid sql injection"""
 import sys
 import 	MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(sys.argv[4]))
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+
+    cur.execute("SELECT id, name FROM states")
+    results = cur.fetchall()
+    for row in results:
+        if row[1] == sys.argv[4]:
+            print(row)
