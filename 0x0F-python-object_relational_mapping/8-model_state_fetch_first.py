72aafd5ef93534f71120ba5b21964618e952049c
MITCHELL OTIENO MONDA <88305630+mitch-codes@users.noreply.github.com>
2023-09-25 13:37:14 +0300

    donno

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -9,19 +9,11 @@ from model_state import Base, State
 
 
 if __name__ == "__main__":
-    engine = create_engine(
-                            'mysql+mysqldb://{}:{}@localhost/{}'
-                            .format(
-                                        sys.argv[1],
-                                        sys.argv[2],
-                                        sys.argv[3]
-                                            ),
-                            pool_pre_ping=True
-                                )
-    session = Session(engine)
-    re = session.query(State).order_by(State.id).first()
-    if re is None:
-        print("Nothing")
-    else:
-        print("{}: {}".format(re.id, re.name))
-    session.close()
+    engine = create_engine("mysql://"+sys.argv[1]+":"+sys.argv[2]+"@localhost:3306/"+sys.argv[3])
+    session = sessionmaker()
+    myrow = session.query(State.id, State.name).order_by(State.id).first()
+    for row in myrow:
+        if (row is not null):
+            print("%s: %s" % (row.id, row.name))
+        else:
+            print()
