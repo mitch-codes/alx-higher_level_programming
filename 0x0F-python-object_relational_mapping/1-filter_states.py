cee974c30635735d54330edf4f550c749827372b
MITCHELL OTIENO MONDA <88305630+mitch-codes@users.noreply.github.com>
2023-09-25 11:17:00 +0300

    add documentation

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""selct id and name where name follows pattern
+"""script that lists all states with a name starting with N
+(upper N) from the database hbtn_0e_0_usa
 """
 import MySQLdb
 import sys
