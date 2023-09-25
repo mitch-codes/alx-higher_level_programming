3c25b3555e17a30204e27203006960df621c7264
MITCHELL OTIENO MONDA <88305630+mitch-codes@users.noreply.github.com>
2023-09-25 11:08:13 +0300

    change syntax

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-"""connect to database python"""
+"""selct id and name where name follows pattern"""
 import 	MySQLdb
 import sys
 
+db = MySQLdb.connect(user=root, passwd=PASS, port=3306, db=hbtn_0e_0_usa)
+cur = db.cursor()
+
+cur.execute("SELECT id, name FROM states WHERE name LIKE %s ORDER BY id",("N%"))
+rows = cur.fetchall()
+for row in rows:
+    print(row)
+
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+    main()
