77eee6e74129af48479f83cb8917336276c492cf
MITCHELL OTIENO MONDA <88305630+mitch-codes@users.noreply.github.com>
2023-09-25 13:25:33 +0300

    change syntax

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -3,19 +3,14 @@
 only one record"""
 import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-from model_state import Base, State
+from base_state import Base, State
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    engine = create_engine(
-                            'mysql+mysqldb://{}:{}@localhost/{}'
-                            .format(
-                                        sys.argv[1],
-                                        sys.argv[2],
-                                        sys.argv[3]
-                                            ),
-                            pool_pre_ping=True
-                                )
-    session = Session(engine)
-    for state in session.query(State).order_by(State.id).all():
-        print("{}: {}".format(state.id, state.name))
+    engine = create_engine(sys.argv[1]+"://"+sys.argv[2]":"+sys.argv[3]+"password@localhost/db")
+    session = sessionmaker(bind=engine)
+
+    myStates = session.query(State).order_by(State.id)
+    for row in myStates:
+        print("%s: %s"% (row.id, row.name))
+
