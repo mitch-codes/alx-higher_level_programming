5994c86527a291f99fc6d02a3af4895a1a04ae67
MITCHELL OTIENO MONDA <88305630+mitch-codes@users.noreply.github.com>
2023-09-25 12:17:39 +0300

    learn how arrays work

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -4,7 +4,7 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[0], passwd=sys.argv[1], db=sys.argv[2])
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
 
     cur.execute("SELECT states.id, cites.name, states.name FROM states INNER JOIN cities WHERE states.id = cities.state_id ORDER BY state.id")
