54e80a73159f344bb901f7e3adc28320790b27b2
MITCHELL OTIENO MONDA <88305630+mitch-codes@users.noreply.github.com>
2023-09-25 10:52:17 +0300

    add documentation

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""connect to mysql db"""
+# Lists all states from the database hbtn_0e_0_usa.
+# Usage: ./0-select_states.py <mysql username> \
+#                             <mysql password> \
+#                             <database name>
 import sys
 import MySQLdb
 
