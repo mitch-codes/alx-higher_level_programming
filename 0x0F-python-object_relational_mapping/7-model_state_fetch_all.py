db83258ca85f481c5870b5ed587e759fb887bb97
MITCHELL OTIENO MONDA <88305630+mitch-codes@users.noreply.github.com>
2023-09-25 13:27:58 +0300

    fix url

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,7 +7,7 @@ from base_state import Base, State
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    engine = create_engine(sys.argv[1]+"://"+sys.argv[2]":"+sys.argv[3]+"password@localhost/db")
+    engine = create_engine(mysql://"+sys.argv[1]":"+sys.argv[2]+"@localhost/"+sys.argv[3])
     session = sessionmaker(bind=engine)
 
     myStates = session.query(State).order_by(State.id)
