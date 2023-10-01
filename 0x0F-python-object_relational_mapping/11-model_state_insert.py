f020e9dcf512d72c72e2d213dc43618b186d7b0d
MITCHELL OTIENO MONDA <88305630+mitch-codes@users.noreply.github.com>
2023-10-01 09:24:37 +0300

    change syntax

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -7,11 +7,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(sys.argv[1], sys.argv[2], sys.argv[3]))
-Session = sessionmaker(bind=engine)
-session = Session()
-louis = State()
-louis.name = "Louisiana"
-session.add(louis)
-print(louis.id)
-session.commit()
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    louis = State()
+    louis.name = "Louisiana"
+    session.add(louis)
+    print(louis.id)
+    session.commit()
