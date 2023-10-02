df24987e36e548dea2e40b45cc6f5220b0edd294
MITCHELL OTIENO MONDA <88305630+mitch-codes@users.noreply.github.com>
2023-10-02 10:40:16 +0300

    create city model

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+from model_state import Base, State
+
+class City(Base):
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+    state = relationship("State", back_populates="cities")
+  
